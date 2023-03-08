57e2e74ece856feb881e6e3439e896565aef8a74
ndphong812 <ndphong812@gmail.com>
2023-03-08 13:09:10 +0700

    create header

--- front-end/src/components/header/index.tsx
+++ front-end/src/components/header/index.tsx
@@ -1,9 +1,24 @@
+import { Link } from "react-router-dom";
+import "./style.scss";
+
 const Header = () => {
     return (
         <>
             <div className="header">
                 <div className="container">
-                    Header Component
+                    <div className="header-main">
+                        <div className="header-main-logo">
+                            <Link to="/">
+                                <img src="https://preview.colorlib.com/theme/electro/img/logo.png"/>
+                            </Link>
+                        </div>
+                        <div className="header-main-search">
+                            Search
+                        </div>
+                        <div className="header-main-user">
+                            User
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
