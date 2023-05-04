0f0ec0ae09844b93e04bdabaf3852e4f20d6b524
caoqui <qui7d18@gmail.com>
2023-05-04 21:24:10 +0700

    update profile user

--- back-end/src/controllers/UserController.ts
+++ back-end/src/controllers/UserController.ts
@@ -3,6 +3,8 @@ import { getRepository } from "typeorm";
 import { validate } from "class-validator";
 const { v4: uuidv4 } = require('uuid');
 
+import * as bcrypt from "bcryptjs";
+
 import { User } from "../entities/User";
 
 class UserController {
@@ -41,18 +43,18 @@ class UserController {
         user.role = role || "";
 
         //check email not null and contain @ char.
-        if(!user.email.includes("@")) {
-            return res.status(409).json({status: "failure", message: "Định dạng email không đúng."});
+        if (!user.email.includes("@")) {
+            return res.status(409).json({ status: "failure", message: "Định dạng email không đúng." });
         }
 
         //check length password >= 6 chars
         if (user.hashpass.length < 6) {
-            return res.status(409).json({status: "failure", message: "Password phải ít nhất 6 kí tự."});
+            return res.status(409).json({ status: "failure", message: "Password phải ít nhất 6 kí tự." });
         }
 
         // check role != admin 
         if (user.role === "admin") {
-            return res.status(401).json({status: "failure", message: "Bạn không thể thiết lập 1 admin mới."});
+            return res.status(401).json({ status: "failure", message: "Bạn không thể thiết lập 1 admin mới." });
         }
 
         user.user_id = uuidv4(); // tạo mã duy nhất
@@ -63,18 +65,18 @@ class UserController {
         try {
             await userRepository.save(user);
         } catch (err) {
-            return res.status(409).json({status: "failure", message: "Email này đã được sử dụng."});
+            return res.status(409).json({ status: "failure", message: "Email này đã được sử dụng." });
         }
 
         //If all ok, send 201 response
-        return res.status(201).json({status: "success", message: "Đã tạo tài khoản thành công."});
+        return res.status(201).json({ status: "success", message: "Đã tạo tài khoản thành công." });
     };
 
     static editUser = async (req: Request, res: Response) => {
 
         const { user_id, email, role } = req.body;
 
-        if(!user_id) {
+        if (!user_id) {
             return res.status(401).json({ status: "failure", message: "Thông tin người dùng cần cập nhật không chính xác." });
         }
 
@@ -143,6 +145,55 @@ class UserController {
             return res.status(404).json({ status: "failure", message: "Người dùng này không tồn tại." });
         }
     };
+
+    static updateProfile = async (req: Request, res: Response) => {
+        const newValues: Partial<User> = req.body;
+        const updateProductRepository = await getRepository(User);
+
+        newValues.user_id = newValues.user_id || "";
+
+        try {
+            await updateProductRepository.findOneOrFail({
+                where: { user_id: newValues.user_id }
+            });
+        } catch (error) {
+            return res.status(401).json({ status: "failure", message: "Người dùng không tồn tại." });
+        }
+
+        if ("role" in newValues) {
+            delete newValues.role;
+        }
+
+
+        if ("email" in newValues) {
+            delete newValues.email;
+        }
+
+        newValues.hashpass = newValues.hashpass || "";
+        if (newValues.hashpass === "") {
+            delete newValues.hashpass;
+        } else {
+            //check length password >= 6 chars
+            if (newValues.hashpass.length < 6) {
+                return res.status(409).json({ status: "failure", message: "Password phải ít nhất 6 kí tự." });
+            }
+            newValues.hashpass = await bcrypt.hashSync(newValues.hashpass, 8);
+        }
+
+
+        try {
+            await updateProductRepository.update(
+                { user_id: newValues.user_id },
+                newValues,
+            );
+
+            console.log("Newvalue_profile: ", newValues);
+
+            return res.status(200).json({ status: "success", message: "Cập nhật hồ sơ cá nhân thành công." });
+        } catch (error) {
+            return res.status(401).json({ status: "failure", message: "Cập nhật hồ sơ cá nhân thất bại." });
+        }
+    }
 };
 
 export default UserController;
